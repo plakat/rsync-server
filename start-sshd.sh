2591f203a13b8c931b2789ae96100cde214b2578
Wolfram Saringer <w.saringer@bsg.at>
2018-04-11 09:17:34 +0200

    use start script to correct .ssh permissions

--- start-sshd.sh
+++ start-sshd.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-chown -R rsyncuser /data
+chown -R rsyncuser:rsyncuser /data /home/rsyncuser
+chmod -R 700 /home/rsyncuser/.ssh
+chown -R root:root /root
+chmod -R 700 /root/.ssh
+
+# dont detach, log to stderr, dont check file permissions:
 /usr/sbin/sshd -D -e -oStrictModes=no
